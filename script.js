83dfc5941e028fc3179cd8ecc3896bc3840cd971
ArturoVa <85954822+ArturoVa@users.noreply.github.com>
2022-07-07 17:15:54 -0500

    Add  opening and closing popup

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let myLibrary = [];
 
 function Book(title, author, pages, read){
@@ -9,12 +8,28 @@ function Book(title, author, pages, read){
 
     this.printInfo= function(){
         let readMessage = this.read ? "already read" : "not read yet";
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${readMessage}`
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readMessage}.`
     }
 }
 
+function openPopUp(){
+    const popUp =document.getElementById("popUp");
+    popUp.style.display = 'block';
 
 
-var book = new Book("hi", "juan", 16,false);
-console.log(book);
-console.log(book.printInfo());
+    
+}
+function addBookToLibrary(){
+
+}
+function closePopUp(){
+    const popUp = document.getElementById("popUp");
+    popUp.style.display= 'none';
+}
+
+const addBookBTN = document.getElementById('add-book');
+const closeButton = document.getElementById('close');
+
+addBookBTN.addEventListener('click',openPopUp);
+closeButton.addEventListener('click', closePopUp)
+
